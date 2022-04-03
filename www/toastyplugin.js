b38f98cb4a26a21771d653a9a27838ad6d7c2456
shimamorieiki <aifore.2270@gmail.com>
2022-04-03 14:30:18 +0900

    revert

--- www/toastyplugin.js
+++ www/toastyplugin.js
@@ -1,41 +1,21 @@
-// // Empty constructor
-// function ToastyPlugin() {}
+// Empty constructor
+function ToastyPlugin() {}
 
-// // The function that passes work along to native shells
-// // Message is a string, duration may be 'long' or 'short'
-// ToastyPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
-//   var options = {};
-//   options.message = message;
-//   options.duration = duration;
-//   cordova.exec(successCallback, errorCallback, 'ToastyPlugin', 'show', [options]);
-// }
-
-// // Installation constructor that binds ToastyPlugin to window
-// ToastyPlugin.install = function() {
-//   if (!window.plugins) {
-//     window.plugins = {};
-//   }
-//   window.plugins.toastyPlugin = new ToastyPlugin();
-//   return window.plugins.toastyPlugin;
-// };
-// cordova.addConstructor(ToastyPlugin.install);
-
-class ToastyPlugin {
-  show(message, duration, successCallback, errorCallback) {
-    var options = {};
-    options.message = message;
-    options.duration = duration;
-    cordova.exec(successCallback, errorCallback, 'ToastyPlugin', 'show', [options]);
-  }
-  
-  install() {
-    if (!window.plugins) {
-      window.plugins = {};
-    }
-    window.plugins.toastyPlugin = new ToastyPlugin();
-    return window.plugins.toastyPlugin;
-  };
-
+// The function that passes work along to native shells
+// Message is a string, duration may be 'long' or 'short'
+ToastyPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
+  var options = {};
+  options.message = message;
+  options.duration = duration;
+  cordova.exec(successCallback, errorCallback, 'ToastyPlugin', 'show', [options]);
 }
 
-cordova.addConstructor(ToastyPlugin.install());
+// Installation constructor that binds ToastyPlugin to window
+ToastyPlugin.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.toastyPlugin = new ToastyPlugin();
+  return window.plugins.toastyPlugin;
+};
+cordova.addConstructor(ToastyPlugin.install);
