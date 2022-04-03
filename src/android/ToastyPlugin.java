c25c17e8fc810447f9a362da3c813ad8b012af71
shimamorieiki <aifore.2270@gmail.com>
2022-04-03 15:34:51 +0900

    use switch

--- src/android/ToastyPlugin.java
+++ src/android/ToastyPlugin.java
@@ -1,4 +1,5 @@
 package com.stanleyidesis.cordova.plugin;
+
 // Cordova-required packages
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -6,41 +7,43 @@ import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 // The native Toast API
 import android.widget.Toast;
 
 public class ToastyPlugin extends CordovaPlugin {
   private static final String DURATION_LONG = "long";
+
   @Override
-  public boolean execute(String action, JSONArray args,
-    final CallbackContext callbackContext) {
-      // Verify that the user sent a 'show' action
-      if (!action.equals("show")) {
+  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) {
+    switch (action) {
+      case "show": {
+        String message;
+        String duration;
+
+        try {
+          JSONObject options = args.getJSONObject(0);
+          message = options.getString("message");
+          duration = options.getString("duration");
+        } catch (JSONException e) {
+          callbackContext.error("Error encountered: " + e.getMessage());
+          return false;
+        }
+
+        // Create the toast
+        Toast toast = Toast.makeText(cordova.getActivity(), message,
+            DURATION_LONG.equals(duration) ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT);
+        // Display toast
+        toast.show();
+        // Send a positive result to the callbackContext
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+        callbackContext.sendPluginResult(pluginResult);
+        return true;
+      }
+
+      default: {
         callbackContext.error("\"" + action + "\" is not a recognized action.");
         return false;
       }
-
-      String message;
-      String duration;
-
-      try {
-        JSONObject options = args.getJSONObject(0);
-        message = options.getString("message");
-        duration = options.getString("duration");
-    } catch (JSONException e) {
-        callbackContext.error("Error encountered: " + e.getMessage());
-        return false;
-      }
-
-      // Create the toast
-      Toast toast = Toast.makeText(cordova.getActivity(), message,
-        DURATION_LONG.equals(duration) ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT);
-      // Display toast
-      toast.show();
-      // Send a positive result to the callbackContext
-      PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
-      callbackContext.sendPluginResult(pluginResult);
-      return true;
+    }
   }
 }
